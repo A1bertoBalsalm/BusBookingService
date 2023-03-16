9f2eb2072d7f850ca28c685517a2630733f24cd8
William <william.ekman@elev.ga.ntig.se>
2023-03-16 22:07:30 +0100

    FIXA BUBBLE SORT AAAAAAAAAA

--- BusBookingService/Program.cs
+++ BusBookingService/Program.cs
@@ -19,7 +19,9 @@ namespace BusBookingService
         //Columm 2 = Personnummer
         //Columm 3 = Kön
         //Columm 4 = Ålder
-        static String[,] BusSeats = new string[21, 4];
+        static String[,] BusSeats = new string[21, 5];
+
+        static int[] BusSeatsSorted = new int[21];
 
         static int free = 0;
         static int freewindowseats = 0;
@@ -54,10 +56,11 @@ namespace BusBookingService
                     Book();
                     break;
                 case 2:
-
+                    CancelBooking();
 
                     break;
                 case 3:
+                    BusData();
 
                     break;
                 case 4:
@@ -72,8 +75,10 @@ namespace BusBookingService
 
         }
 
-     
 
+        // BOOKING //
+
+       
         static void Book()
         {
 
@@ -91,27 +96,28 @@ namespace BusBookingService
             switch (Convert.ToInt32(BookChoice))
             {
                 case 1:
-                    SearchBooking(true);
-                    Console.WriteLine("dumbom");
+                    if (freewindowseats > 0)
+                    {
+                        SearchBooking(true);
+                        Console.WriteLine("dumbom");
+                    }
+                    else
+                    {
+                        // FIXA SENARE
+                    }
                     break;
                 case 2:
                     SearchBooking(false);
 
                     break;
                 case 3:
-                    Console.WriteLine("dum");
+                    Meny();
                     break;
 
                 default:
                     Meny();
                     break;
             }
-
-
-
-
-
-
 
         }
 
@@ -183,28 +189,28 @@ namespace BusBookingService
             string SSN = Console.ReadLine();
 
 
-            BusSeats[i, 0] = Name;
+            BusSeats[i, 0] = Name.ToLower(); 
 
-            BusSeats[i, 1] = Lastname;
+            BusSeats[i, 1] = Lastname.ToLower(); 
 
-            BusSeats[i, 3] = Gender;
+            BusSeats[i, 3] = Gender; 
 
-            BusSeats[i, 2] = SSN;
+            BusSeats[i, 2] = SSN; 
 
           
             int Year = Convert.ToInt32(SSN.Substring(0,4));
             int Month = Convert.ToInt32(SSN.Substring(4,2));
             int Day = Convert.ToInt32(SSN.Substring(6,2));
 
-            var born = new DateTime(Year, Month, Day); //15 July 2021
+            var born = new DateTime(Year, Month, Day);
             var today = DateTime.Now;
             var diffOfDates = today - born;
 
             int Age = Convert.ToInt32(diffOfDates.TotalDays) / 365;
 
-            BusSeats[i, 3] =  Age.ToString();
+            BusSeats[i, 4] =  Age.ToString();
 
-            Console.WriteLine("Din ålder "+Age);
+            
 
             Double Pris;
 
@@ -218,6 +224,9 @@ namespace BusBookingService
             }
             else if(Age >=18 && Age > 69) 
             {
+
+                // BRAM VARFÖR HAR DU INTE BARA DEN HÄR PÅ ELSE !!!!!!!!!!!!!!!
+
                 Console.WriteLine("du är myndig");
                 Adults++;
                 Pris = AdultsPrice;
@@ -248,30 +257,178 @@ namespace BusBookingService
                     Console.WriteLine("ost");
                     break;  
 
-
-
-                 
             }
 
 
+        }
+
+
+        // CANCEL BOOKING
+
+
+        private static void CancelBooking()
+        {
+            Console.WriteLine("1. Avboka med ditt förnamn 2. Avboka med ditt Personnummer 3. Menu");
+            String Cancel = Console.ReadLine();
+            switch (Convert.ToInt16(Cancel))
+            {
+                case 1:
+                    Console.WriteLine("Skriv Ditt Namn");
+                    String Name = Console.ReadLine();
+
+                    SearchUser(Name, 0);
+                    break;
+                case 2:
+                    Console.WriteLine("Skriv Ditt personnummer");
+                    String SSN = Console.ReadLine();
+
+                    SearchUser(SSN, 2);
+                    break;
+                case 3:
+                    Meny();
+                    break;
+                default:
+                    break;
+            }
+
+            
+           
+        }
+
+        static void SearchUser(String DataSearch, int Datatype)
+        {
+            DataSearch = DataSearch.ToLower();
+            //Columm 0 = Förnamn
+            //Columm 1 = Efternamn
+            //Columm 2 = Personnummer 
+
+            int[] FindBooked = new int[21];
+
+
+            int j = 0;
+
+            for (int i = 0; i < 21; i++)
+            {
+                if (DataSearch == BusSeats[i, Datatype])
+                {
+                    FindBooked[j] = i;
+                    j++;
+
+
+                }
+
+            }
+            Console.WriteLine("Ostbåge");
+            for (int i = 0;i < j;i++) {
+
+                Console.WriteLine("Säte: " + FindBooked[i] +" Namn: "+BusSeats[FindBooked[i], 0]+" Personnummer: "+BusSeats[FindBooked[i],2]);
+                Console.WriteLine("VARNING: ÄR DETTA DIN PLATS Y/N, N: Gå till nästa match");
+                String ChooseYourSeat = Console.ReadLine().ToLower();
+                if (ChooseYourSeat == "y")
+                {
+                    int AgeRemovePrice = Convert.ToInt32(BusSeats[FindBooked[i], 5]);
+                    if (AgeRemovePrice < 18)
+                    {
+                        
+                        Kids--;
+                        
+
+
+                    }
+                    else if (AgeRemovePrice >= 18 && AgeRemovePrice > 69)
+                    {
+                        // HÄR OCKSÅ!!!
+                        
+                        Adults--;
+                        
+
+                    }
+                    else
+                    {
+         
+                        Elderly--;
+                    }
+
+
+                    Booked[FindBooked[i]] = false;
+ 
+                    Console.WriteLine("Avbokad");
+
+
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            
 
         }
+
+        static void BusData()
+        {
+            BubbleSort();
+
+            // Output the header.
+            Console.Write("Plats".PadRight(15));
+            Console.Write("Förnamn".PadRight(15));
+            Console.Write("Efternamn".PadRight(17));
+            Console.Write("Personnummer".PadRight(20));
+            Console.Write("Kön".PadRight(11));
+            Console.WriteLine("Ålder");
+
+            // Output array.
+            for (int i = 0; i < 21; i++)
+            {
+                if(true == Booked[i])
+                {
+                    Console.Write(BusSeats[BusSeatsSorted[i], i].PadRight(15));
+                    Console.Write(BusSeats[BusSeatsSorted[i], 0].PadRight(15));
+                    Console.Write(BusSeats[BusSeatsSorted[i], 1].PadRight(17));
+                    Console.Write(BusSeats[BusSeatsSorted[i], 2].PadRight(20));
+                    Console.Write(BusSeats[BusSeatsSorted[i], 3].PadRight(11));
+                    Console.WriteLine(BusSeats[BusSeatsSorted[i], 4]);
+
+                }
+             
+                
+            }
+
+       
+        }
+        static void BubbleSort()
+        {
+
+            for (int i = 0;i < 21;i++)
+            {
+                if(true == Booked[i])
+                {
+                    BusSeatsSorted[i] = Convert.ToInt16(BusSeats[i, 4]);
+                }
+                
+            }
+            int temp = 0;
+
+            for (int write = 0; write < BusSeatsSorted.Length; write++)
+            {
+                for (int sort = 0; sort < BusSeatsSorted.Length - 1; sort++)
+                {
+                    if (BusSeatsSorted[sort] > BusSeatsSorted[sort + 1])
+                    {
+                        temp = BusSeatsSorted[sort + 1];
+                        BusSeatsSorted[sort + 1] = BusSeatsSorted[sort];
+                        BusSeatsSorted[sort] = temp;
+                    }
+                }
+            }
+
+        }
+
 
 
     }
